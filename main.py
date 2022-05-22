28409fa4109306cb3dd8a347ff99047e159e0c23
Erizon18 <61296141+Erizon18@users.noreply.github.com>
2022-05-22 15:20:55 -0300

    Change to OOP and added components and World

--- main.py
+++ main.py
@@ -1,148 +1,268 @@
-from cgitb import reset
-from pickletools import float8, long1
 import random
-from tokenize import Double
+
 import pygame
 import sys
 
 # Inicialização
 pygame.init()
-clock = pygame.time.Clock()
+
 # Configurando a janela
 screenWidth = 1280
 screenHeight = 960
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('Pong')
-# Objetos
-ball = pygame.Rect(screenWidth / 2 - 15, screenHeight / 2 - 15, 30, 30)
-player = pygame.Rect(screenWidth - 20, screenHeight / 2 - 70, 10, 140)
-opponent = pygame.Rect(10, screenHeight / 2 - 70, 10, 140)
-# variaveis
-ballSpeedX = 0.5  # 500 pixels por segundo
-ballSpeedY = 0.5
-opponentSpeed = 10
 
 
-def inputs():
-    global gameStarted
-    # Processando as entradas (eventos)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            gameStarted = True
-    (x, y) = pygame.mouse.get_pos()
-    player.y = y - 70
+#Classes
 
 
-def draw():
-    global font, player1Point, player2Point, gameStarted, gameEnded
-    
-    # Desenho
-    if(gameStarted and not gameEnded):
+
+class Entity:
+    def __init__(self):
+        self.physics = PhysicsComponent()
+        self.graphics = GraphicsComponent()
+
+    def update(self, world, dt):
+        pass
+
+    def draw(self, dt):
+        pass
+
+
+class Component():
+    def __init__(self, parent):
+        self.parent = parent
+
+
+class PhysicsComponent(Component):
+    def update(self, world, dt):
+        pass
+
+
+class GraphicsComponent(Component):
+    def draw(self, dt):
+        pass
+
+
+class World(Entity):
+    def __init__(self) -> None:
+        self.physics_entities = []
+        self.singleton_entities = []
+        self.ball = Ball()
+        self.player = Player(self.ball)
+        self.opponent = Opponent(self.ball)
+        self.judge = Judge()
+
+        self.physics_entities.append(self.ball)
+        self.physics_entities.append(self.player)
+        self.physics_entities.append(self.opponent)
+        self.singleton_entities.append(self.judge)
+
+    def inputs(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def update(self, dt):
+        for e in self.physics_entities:
+            e.update(self, dt)
+
+    def draw(self, dt):
         screen.fill((0, 0, 0))
-        pygame.draw.ellipse(screen, (200, 200, 200), ball)
-        pygame.draw.rect(screen, (200, 200, 200), player)
-        pygame.draw.rect(screen, (200, 200, 200), opponent)
-        point1 = font.render(str(player1Point), True, pygame.Color(255, 255, 255))
-        point2 = font.render(str(player2Point), True, pygame.Color(255, 255, 255))
-        screen.blit(point1, (screenWidth / 2 - 100, 100))
-        screen.blit(point2, (screenWidth / 2 + 100, 100))
+        for e in self.singleton_entities:
+            e.draw(dt)
+        for e in self.physics_entities:
+            e.draw(dt)
         # Atualizando a janela 60fps
         pygame.display.flip()
 
-    if(gameEnded):
-        screen.fill((0, 0, 0))
-        gameOver = font.render("Game Over", True, pygame.Color(255, 255, 255))
-        screen.blit(gameOver, (screenWidth / 2 - (gameOver.get_width() / 2), screenHeight / 2 - (gameOver.get_height() / 2)))
-        pygame.display.flip()
 
-    elif(not gameStarted):
-        screen.fill((0, 0, 0))
-        pong = font.render("Pong", True, pygame.Color(255, 255, 255))
-        start = font.render("Clique com o mouse para iniciar", True, pygame.Color(255, 255, 255))
-        screen.blit(pong, (screenWidth / 2 - (pong.get_width() / 2), screenHeight / 2 - 200))
-        screen.blit(start, (screenWidth / 2 - (start.get_width() / 2), screenHeight / 2))
-        pygame.display.flip()
+class Ball(Entity):
+    def __init__(self):
+        self.body = pygame.Rect(screenWidth / 2 - 15, screenHeight / 2 - 15, 30, 30)
+        self.speedX = 0.4
+        self.speedY = 0.4
 
+        self.physics = BallPhysicsComponent(self)
+        self.graphics = BallGraphicsComponent(self)
 
-def resetBall():
-    global ballSpeedX, ballSpeedY
+    def update(self, world, dt):
+        self.physics.update(world, dt)
+    
+    def draw(self, dt):
+        self.graphics.draw(dt)
+            
+    def resetBall(self):
+        self.body.center = (screenWidth / 2, screenHeight / 2)
+        self.speedX = random.choice((-0.5, 0.5))
 
-    ball.center = (screenWidth / 2, screenHeight / 2)
-    ballSpeedX = random.choice((-0.5, 0.5))
-
-
-def update(dt):
-    global ballSpeedX, ballSpeedY, player1Point, player2Point, gameStarted, gameEnded, calc
-
-    # Atualização
-    if(gameStarted and not gameEnded):
-        ball.x += ballSpeedX * dt
-        ball.y += ballSpeedY * dt
-        if opponent.bottom < ball.y:
-            opponent.bottom += opponentSpeed
-        if opponent.top > ball.y:
-            opponent.top -= opponentSpeed
-
-        if (ball.y <= 0 or ball.bottom >= screenHeight):
-            ballSpeedY *= -1
-            print(ballSpeedY)
-        
-        if ball.bottom >= opponent.top and ball.top <= opponent.bottom and ball.left <= opponent.right:
-            delta = ball.centery - opponent.centery
-            ballSpeedY = delta * 0.01
-            if ballSpeedY < 0 and ballSpeedY * dt < -1:
-                ballSpeedY = -1 / dt
-            elif ballSpeedY > 0 and ballSpeedY * dt < 1:
-                ballSpeedY = 1 / dt
-            ballSpeedX *= -1
-        elif ball.bottom >= player.top and ball.top <= player.bottom and ball.right >= player.left:
-            delta = ball.centery - player.centery
-            ballSpeedY = delta * 0.01
-            if ballSpeedY < 0 and ballSpeedY * dt > -1:
-                ballSpeedY = -1 / dt
-            elif ballSpeedY > 0 and ballSpeedY * dt < 1:
-                ballSpeedY = 1 / dt
-            ballSpeedX *= -1
-
-        if ball.left >= screenWidth:
-            player1Point += 1
-            if(player1Point < 5):
-                resetBall()
-            elif(player1Point >= 5):
-                gameEnded = True
-        elif ball.right <= 0:
-            player2Point +=1
-            if(player2Point < 5):
-                resetBall()
-            elif(player2Point >= 5):
-                gameEnded = True    
+    def change_course(self, dt):
+        self.speedY = dt * 0.01
+        self.speedX *= -1
 
 
+class BallPhysicsComponent(PhysicsComponent):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.body = parent.body
+
+    def update(self, world, dt):
+        self.body.x = self.body.x + self.parent.speedX * dt
+        self.body.y = self.body.y + self.parent.speedY * dt
+        if self.body.top <= 0 or self.body.bottom >= screenHeight:
+            self.parent.speedY = -self.parent.speedY
+        if self.body.left > screenWidth or self.body.right < 0:
+            for e in world.singleton_entities:
+                if(self.body.left > screenWidth):
+                    e.change_score(False)
+                elif(self.body.right < 0):
+                    e.change_score(True)
+            self.parent.resetBall()
+
+
+
+class BallGraphicsComponent(GraphicsComponent):   
+    def draw(self, dt):
+        pygame.draw.ellipse(screen, (200, 200, 200), self.parent.body)
+
+
+class Player(Entity):
+    def __init__(self, ball):
+        self.body = pygame.Rect(screenWidth - 20, screenHeight / 2 - 70, 10, 140)
+        self.score = 0
+
+        self.physics = PlayerPhysicsComponent(self)
+        self.graphics = PlayerGraphicsComponent(self)
+
+    def update(self, world, dt):
+        self.physics.update(world, dt)
+
+    def draw(self, dt):
+        self.graphics.draw(dt)
+
+
+class PlayerPhysicsComponent(PhysicsComponent):
+    def __init__(self, parent):
+        self.parent = parent
+        self.body = parent.body
+
+    def update(self, world, dt):
+        for e in world.physics_entities:
+            if e == self.parent: 
+                continue
+            if e.body.bottom >= self.body.top and e.body.top <= self.body.bottom and e.body.right >= self.body.left:
+                delta = e.body.centery - self.body.centery
+                e.change_course(delta)
+        (x, y) = pygame.mouse.get_pos()
+        self.body.y = y - 70
+
+
+class PlayerGraphicsComponent(GraphicsComponent):
+    def draw(self, dt):
+        pygame.draw.rect(screen, (200, 200, 200), self.parent.body)
+
+
+class Opponent(Entity):
+    def __init__(self, ball):
+        self.body = pygame.Rect(10, screenHeight / 2 - 70, 10, 140)
+        self.speed = 8
+        self.score = 0
+
+        self._ball = ball
+        self.physics = OpponentPhysicsComponent(self)
+        self.graphics = OpponentGraphicsComponent(self)
+
+    def update(self, world, dt):
+        self.physics.update(world, dt)
+
+    def draw(self, dt):
+        self.graphics.draw(dt)
+
+
+class OpponentPhysicsComponent(PhysicsComponent):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.body = parent.body
+        self._ball = parent._ball
+        self.speed = parent.speed
+
+    def update(self, world, dt):
+        if self.body.bottom < self._ball.body.y:
+            self.body.bottom += self.speed
+        if self.body.top > self._ball.body.y:
+            self.body.top -= self.speed
+            
+        for e in world.physics_entities:
+            if e == self.parent: 
+                continue
+            if e.body.bottom >= self.body.top and e.body.top <= self.body.bottom and e.body.left <= self.body.right:
+                delta = e.body.centery - self.body.centery
+                e.change_course(delta)
+
+
+class OpponentGraphicsComponent(GraphicsComponent):
+    def draw(self, dt):
+        pygame.draw.rect(screen, (200, 200, 200), self.parent.body)
+
+
+class Judge(Entity):
+    def __init__(self):
+        self.player_score = 0
+        self.opponent_score = 0
+
+        self.player_score_text = ScoreTextGraphicsComponent(self, True, (screenWidth / 2 + 120, 200))
+        self.opponent_score_text = ScoreTextGraphicsComponent(self, False, (screenWidth / 2 - 120, 200))
+    
+    def draw(self, dt):
+        self.player_score_text.draw(dt)
+        self.opponent_score_text.draw(dt)
+
+    def change_score(self, is_player):
+        if is_player:
+            self.player_score += 1
+            self.player_score_text.change_score()
+        elif not is_player:
+            self.opponent_score += 1
+            self.opponent_score_text.change_score()
+
+
+class ScoreTextGraphicsComponent(GraphicsComponent):
+    def __init__(self, parent, is_player,coord):
+        super().__init__(parent)
+        self.coord = coord
+        self.font = pygame.font.SysFont('arial', 32)
+        self.is_player = is_player
+        self.text = self.font.render("0", True, pygame.Color(255, 255, 255))
+
+    def change_score(self):
+        if self.is_player:
+            self.text = self.font.render(str(self.parent.player_score), True, pygame.Color(255, 255, 255))
+        elif not self.is_player:
+            self.text = self.font.render(str(self.parent.opponent_score), True, pygame.Color(255, 255, 255))
+
+    def draw(self, dt):
+        screen.blit(self.text, self.coord)
+
+
+#Objetos
+world = World()
 previous = pygame.time.get_ticks()
 lag = 0
-FPS = 500
-MS_PER_UPDATE = 1000/FPS
-player1Point = 0
-player2Point = 0
-gameStarted = False
-gameEnded = False
-calc = 0
-font = pygame.font.SysFont('arial', 32)
-
+FPS = 60
+MS_PER_UPDATE = 1000 / FPS
 while True:
     current = pygame.time.get_ticks()
     elapsed = current - previous
     previous = current
     lag += elapsed
-
-    #Entradas
-    inputs()
+    # Entradas
+    world.inputs()
+    # Atualização
     while lag >= MS_PER_UPDATE:
         # Atualização
-        update(MS_PER_UPDATE)
+        world.update(MS_PER_UPDATE)
         lag -= MS_PER_UPDATE
-    #Desenho
-    draw()
+    # Desenho
+    world.draw(MS_PER_UPDATE)
